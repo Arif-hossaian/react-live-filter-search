8b9a3fd40ca1b60ecfb9db29db72c951c129854f
arifhossain123 <arifhossain42136@gmail.com>
2020-07-07 13:02:46 +0600

    Updates

---
+++ precache-manifest.ef1b36a00112641896934ed2d5520244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73418e6b25b2ab95e222d0b3acac12c8",
+    "url": "/react-live-filter-search/index.html"
+  },
+  {
+    "revision": "d338284134aaa6140989",
+    "url": "/react-live-filter-search/static/css/main.f6127ec1.chunk.css"
+  },
+  {
+    "revision": "3211df2fee1580d430ce",
+    "url": "/react-live-filter-search/static/js/2.a6a19e9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-live-filter-search/static/js/2.a6a19e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d338284134aaa6140989",
+    "url": "/react-live-filter-search/static/js/main.528ea8b1.chunk.js"
+  },
+  {
+    "revision": "875c026f26cdf51b1189",
+    "url": "/react-live-filter-search/static/js/runtime-main.dcb3c08a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-live-filter-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
